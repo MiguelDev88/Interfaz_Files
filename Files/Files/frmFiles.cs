4559281537065b9176d274f25a5f7a8bda0906ec
a15carlosmpf <a15carlosmpf@W10D-I4E20>
2017-02-02 09:03:52 +0100

    subiendo

---
+++ Files/Files/frmFiles.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Files
+{
+    public partial class frmFiles : Form
+    {
+        public frmFiles()
+        {
+            InitializeComponent();
+            
+            
+        }
+
+        private void Menu_MouseHover(object sender, EventArgs e)
+        {
+            labelHelp.Text = ((ToolStripMenuItem)sender).Text;   
+        }
+
+        private void frmFiles_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
